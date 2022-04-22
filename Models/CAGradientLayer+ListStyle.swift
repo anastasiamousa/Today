4faabb9141736af895f9ed663e5500333a2b560c
Anastasia Mousa <49337566+anastasiamousa@users.noreply.github.com>
2022-04-22 14:00:52 +0300

    gradient background

---
+++ Models/CAGradientLayer+ListStyle.swift
@@ -0,0 +1,35 @@
+//
+//  CAGradientLayer+ListStyle.swift
+//  Today
+//
+//  Created by Anastasia Mousa on 22/4/22.
+//
+
+import UIKit
+
+extension CAGradientLayer {
+    static func gradientLayer(for style: ReminderListStyle, in frame: CGRect) -> Self
+    {
+        let layer = Self()
+        layer.colors = colors(for: style)
+        layer.frame = frame
+        return layer
+    }
+    private static func colors (for style: ReminderListStyle) -> [CGColor] {
+        let beginColor: UIColor
+        let endColor: UIColor
+        
+        switch style {
+        case .today:
+            beginColor = .todayGradientTodayBegin
+            endColor = .todayGradientTodayEnd
+        case .future:
+            beginColor = .todayGradientFutureBegin
+            endColor = .todayGradientFutureEnd
+        case .all:
+            beginColor = .todayGradientAllBegin
+            endColor = .todayGradientAllEnd
+        }
+        return [beginColor.cgColor, endColor.cgColor]
+    }
+}
